9e32f96ddae41ac0aacb4d64a98b76959d356f4d
Vitaliy Potapov <noginsk@rambler.ru>
2021-04-30 16:05:53 +0300

    init

---
+++ src/request.ts
@@ -0,0 +1,53 @@
+// See: https://yandex.ru/dev/dialogs/alice/doc/protocol.html#request
+
+import { Nlu } from './nlu';
+
+export interface ReqBody {
+  meta: Meta;
+  request: Request;
+  session: Session;
+  state?: State;
+  version: '1.0';
+}
+
+export interface Meta {
+  locale: string;
+  timezone: string;
+  client_id: string;
+  interfaces: {
+    screen?: Record<string, never>;
+    account_linking?: Record<string, never>;
+  }
+}
+
+export interface Request {
+  command: string;
+  original_utterance: string;
+  type: 'SimpleUtterance' | 'ButtonPressed';
+  markup?: {
+    dangerous_context?: true;
+  };
+  payload?: unknown;
+  nlu?: Nlu;
+}
+
+export interface Session {
+  message_id: number;
+  session_id: string;
+  skill_id: string;
+  user_id: string;
+  user?: {
+    user_id: string;
+    access_token: string;
+  };
+  application: {
+    application_id: string;
+  }
+  new: boolean;
+}
+
+export interface State {
+  session?: unknown;
+  application?: unknown;
+  user?: unknown;
+}
---
+++ src/response.ts
@@ -0,0 +1,36 @@
+// See: https://yandex.ru/dev/dialogs/alice/doc/protocol.html#response
+
+import { Card } from './images';
+
+export interface ResBody {
+  response: Response;
+  version: '1.0';
+  analytics?: Analytics;
+  session_state?: unknown;
+  application_state?: unknown;
+  user_state_update?: unknown;
+}
+
+export interface Response {
+  text: string;
+  tts?: string;
+  card?: Card;
+  buttons?: Button[];
+  end_session: boolean;
+}
+
+export interface Button {
+  title: string;
+  url?: string;
+  payload?: unknown;
+  hide?: boolean;
+}
+
+export interface Analytics {
+  events: AnalyticsEvent[];
+}
+
+export interface AnalyticsEvent {
+  name: string;
+  value?: Record<string, unknown>;
+}
